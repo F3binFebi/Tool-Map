578469b567c11a4381dcf5a080ea6907d56d969a
F3B1N <febinfebi003@gmail.com>
2026-09-19 14:12:32 +0530

    Fix React Router base path

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import { Profile } from './pages/Profile';
 
 export const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <CompareProvider>
         <SavedProvider>
           <Routes>
